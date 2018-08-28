95d5b6eeae600bc13bdd1f3ab49704950619b911
Andrew Roth <andrewjlroth@gmail.com>
2018-08-28 14:10:22 -0700

    refactor: cleaning out old code

--- pgfa/inference/feature_matrix/__init__.py
+++
@@ -1,9 +0,0 @@
-from .gibbs import GibbsFeatureAllocationMatrixKernel
-from .row_gibbs import RowGibbsFeatureAllocationMatrixKernel
-from .particle_gibbs import ParticleGibbsFeatureAllocationMatrixKernel
-
-__all__ = [
-    'GibbsFeatureAllocationMatrixKernel',
-    'RowGibbsFeatureAllocationMatrixKernel',
-    'ParticleGibbsFeatureAllocationMatrixKernel'
-]
