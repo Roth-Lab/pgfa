876411eacdc6274e30391bce8abd4ad1a9a045e2
Andrew Roth <andrewjlroth@gmail.com>
2018-10-05 15:21:44 -0700

    refactor: unifiying row gibbs and restricted row gibbs

--- pgfa/updates/feature_matrix/row_gibbs.py
+++ pgfa/updates/feature_matrix/row_gibbs.py
@@ -5,8 +5,17 @@ import numpy as np
 from pgfa.math_utils import discrete_rvs, log_normalize
 
 
-def do_row_gibbs_update(density, a, b, cols, row_idx, data, params):
-    Zs = list(map(np.array, itertools.product([0, 1], repeat=len(cols))))
+def do_restricted_row_gibbs_update(density, a, b, cols, row_idx, data, params, max_cols=1):
+    K = len(cols)
+
+    if K > max_cols:
+        K = max_cols
+
+    update_cols = np.random.choice(cols, replace=False, size=K)
+
+    Zs = np.tile(params.Z[row_idx], (2**K, 1))
+
+    Zs[:, update_cols] = list(map(np.array, itertools.product([0, 1], repeat=K)))
 
     Zs = np.array(Zs, dtype=np.int)
 
