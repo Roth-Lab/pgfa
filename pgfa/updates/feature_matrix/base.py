30bf5348ea7af2fae576abc87f680bfd51c2006d
Andrew Roth <andrewjlroth@gmail.com>
2018-10-17 17:19:57 -0700

    bugfix: singleton updates not writing

--- pgfa/updates/feature_matrix/base.py
+++ pgfa/updates/feature_matrix/base.py
@@ -6,21 +6,17 @@ class FeatureAllocationMatrixUpdater(object):
         self.singletons_updater = singletons_updater
 
     def update(self, model):
-        params = model.params
-
-        num_rows = params.Z.shape[0]
+        num_rows = model.params.Z.shape[0]
 
         for row_idx in get_rows(num_rows):
-            cols = model.feat_alloc_prior.get_update_cols(row_idx, params.Z)
+            cols = model.feat_alloc_prior.get_update_cols(row_idx, model.params.Z)
 
-            feat_probs = model.feat_alloc_prior.get_feature_probs(row_idx, params.Z)
+            feat_probs = model.feat_alloc_prior.get_feature_probs(row_idx, model.params.Z)
 
-            params = self.update_row(cols, model.data, model.data_dist, feat_probs, params, row_idx)
+            model.params = self.update_row(cols, model.data, model.data_dist, feat_probs, model.params, row_idx)
 
             if self.singletons_updater is not None:
-                params = self.singletons_updater.update_row(model, row_idx)
-
-        model.params = params
+                model.params = self.singletons_updater.update_row(model, row_idx)
 
     def update_row(self, cols, data, dist, feat_probs, params, row_idx):
         raise NotImplementedError
