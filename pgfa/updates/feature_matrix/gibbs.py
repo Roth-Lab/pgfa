6643c7e40c585b5a0816a6421b71fcefd03c981e
Andrew Roth <andrewjlroth@gmail.com>
2018-10-17 17:18:34 -0700

    minor: gibbs clean up

--- pgfa/updates/feature_matrix/gibbs.py
+++ pgfa/updates/feature_matrix/gibbs.py
@@ -8,24 +8,21 @@ from .base import FeatureAllocationMatrixUpdater
 
 class GibbsUpdater(FeatureAllocationMatrixUpdater):
     def update_row(self, cols, data, dist, feat_probs, params, row_idx):
-        return do_gibbs_update(data, dist, cols, feat_probs, params, row_idx)
+        log_p = np.zeros(2)
 
+        for k in cols:
+            params.Z[row_idx, k] = 0
 
-@numba.jit
-def do_gibbs_update(data, dist, cols, feat_probs, params, row_idx):
-    log_p = np.zeros(2)
+            log_p[0] = np.log(1 - feat_probs[k])
+            log_p[0] += dist.log_p_row(data, params, row_idx)
 
-    for k in cols:
-        params.Z[row_idx, k] = 0
+            params.Z[row_idx, k] = 1
 
-        log_p[0] = np.log(1 - feat_probs[k]) + dist.log_p_row(data, params, row_idx)
+            log_p[1] = np.log(feat_probs[k])
+            log_p[1] += dist.log_p_row(data, params, row_idx)
 
-        params.Z[row_idx, k] = 1
+            log_p = log_normalize(log_p)
 
-        log_p[1] = np.log(feat_probs[k]) + dist.log_p_row(data, params, row_idx)
+            params.Z[row_idx, k] = discrete_rvs(np.exp(log_p))
 
-        log_p = log_normalize(log_p)
-
-        params.Z[row_idx, k] = discrete_rvs(np.exp(log_p))
-
-    return params
+        return params
