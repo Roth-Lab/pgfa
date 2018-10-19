c3b3e0103294feb1ef722827f1cdee068f0c8280
Andrew Roth <andrewjlroth@gmail.com>
2018-10-19 14:35:30 -0700

    minor: package level imports for updates

---
+++ pgfa/updates/__init__.py
@@ -0,0 +1,5 @@
+from .gibbs import GibbsUpdater
+from .metropolis_hastings import MetropolisHastingsUpdater
+from .mixture import GibbsMixtureUpdater
+from .particle_gibbs import ParticleGibbsUpdater
+from .row_gibbs import RowGibbsUpdater
