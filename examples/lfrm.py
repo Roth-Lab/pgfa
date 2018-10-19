a34bc0a7d14fe6d158d1a4f9ab01fcc7e66957f1
Andrew Roth <andrewjlroth@gmail.com>
2018-10-18 17:44:34 -0700

    refactpr: updating lfrm model

--- examples/lfrm.py
+++ examples/lfrm.py
@@ -1,31 +1,48 @@
 import numpy as np
 
-from pgfa.feature_allocation_priors import BetaBernoulliFeatureAllocationDistribution
 from pgfa.math_utils import bernoulli_rvs
-from pgfa.models.lfrm import LatentFactorRelationalModel, Parameters
-from pgfa.updates.feature_matrix import GibbsMixtureUpdater, ParticleGibbsUpdater
 from pgfa.utils import get_b_cubed_score
+
+import pgfa.feature_allocation_priors
+import pgfa.models.lfrm
+import pgfa.updates.feature_matrix
 
 
 def main():
     np.random.seed(0)
 
     K = 4
-    N = 20
+    N = 100
 
-    feat_alloc_prior = BetaBernoulliFeatureAllocationDistribution(1, 1, K)
+    feat_alloc_prior = pgfa.feature_allocation_priors.BetaBernoulliFeatureAllocationDistribution(10, 1, K)
 
     data, data_true, params = simulate_data(feat_alloc_prior, N)
 
-    feat_alloc_updater = ParticleGibbsUpdater(annealed=True)
+#     singletons_updater = None
 
-    feat_alloc_updater = GibbsMixtureUpdater(feat_alloc_updater)
+    singletons_updater = pgfa.models.lfrm.PriorSingletonsUpdater()
 
-    model = LatentFactorRelationalModel(data, feat_alloc_prior, feat_alloc_updater)
+    feat_alloc_updater = pgfa.updates.feature_matrix.ParticleGibbsUpdater(
+        annealed=True, singletons_updater=singletons_updater
+    )
+
+    feat_alloc_updater = pgfa.updates.feature_matrix.GibbsMixtureUpdater(feat_alloc_updater)
+
+#     feat_alloc_updater = pgfa.updates.feature_matrix.GibbsUpdater(singletons_updater=singletons_updater)
+
+    model_updater = pgfa.models.lfrm.LatentFactorRelationalModelUpdater(feat_alloc_updater)
+
+    feat_alloc_prior = pgfa.feature_allocation_priors.IndianBuffetProcessDistribution()
+
+    model = pgfa.models.lfrm.LatentFactorRelationalModel(data, feat_alloc_prior, symmetric=False)
+
+    print(np.sum(params.Z, axis=0))
+
+    print('@' * 100)
 
     for i in range(10000):
-        if i % 100 == 0:
-            print(i, model.params.K, model.log_p)
+        if i % 10 == 0:
+            print(i, model.params.K, model.log_p, np.sum(np.abs(model.predict() - data_true)), model.params.tau)
 
             if model.params.K > 0:
                 print(get_b_cubed_score(params.Z, model.params.Z))
@@ -34,10 +51,10 @@ def main():
 
             print('#' * 100)
 
-        model.update()
+        model_updater.update(model)
 
 
-def simulate_data(feat_alloc_prior, N, tau=1):
+def simulate_data(feat_alloc_prior, N, tau=0.1):
     Z = feat_alloc_prior.rvs(N)
 
     K = Z.shape[1]
@@ -46,29 +63,41 @@ def simulate_data(feat_alloc_prior, N, tau=1):
     V = np.triu(V)
     V = V + V.T - np.diag(np.diag(V))
 
-    params = Parameters(1, tau, V, Z)
+    params = pgfa.models.lfrm.Parameters(tau, V, Z)
 
     data_true = np.zeros((N, N))
 
+#     for i in range(N):
+#         for j in range(i, N):
+#             if i == j:
+#                 data_true[i, j] = 1
+#
+#             else:
+#                 m = Z[i].T @ V @ Z[j]
+#
+#                 f = np.exp(-m)
+#
+#                 p = 1 / (1 + f)
+#
+#                 data_true[i, j] = bernoulli_rvs(p)
+#
+#                 data_true[j, i] = data_true[i, j]
+
     for i in range(N):
         for j in range(N):
-            if i == j:
-                data_true[i, j] = 1
+            m = Z[i].T @ V @ Z[j]
 
-            else:
-                m = Z[i].T @ V @ Z[j]
+            f = np.exp(-m)
 
-                f = np.exp(-m)
+            p = 1 / (1 + f)
 
-                p = 1 / (1 + f)
-
-                data_true[i, j] = bernoulli_rvs(p)
+            data_true[i, j] = bernoulli_rvs(p)
 
     data = data_true.copy()
 
     for i in range(N):
         for j in range(N):
-            if np.random.random() < 0.1:
+            if np.random.random() < 0.0:
                 data[i, j] = np.nan
 
     return data, data_true, params
--- pgfa/models/lfrm.py
+++ pgfa/models/lfrm.py
@@ -6,17 +6,13 @@ from pgfa.math_utils import bernoulli_rvs, do_metropolis_hastings_accept_reject
 
 
 class LatentFactorRelationalModel(object):
-    def __init__(self, data, feat_alloc_prior, feat_alloc_updater, params=None, priors=None):
+    def __init__(self, data, feat_alloc_prior, params=None, priors=None, symmetric=True):
         self.data = data
-
-        self.feat_alloc_updater = feat_alloc_updater
 
         self.feat_alloc_prior = feat_alloc_prior
 
-        self.data_dist = DataDistribution()
-
         if params is None:
-            params = get_params_from_data(data, feat_alloc_prior)
+            params = self.get_params_from_data()
 
         self.params = params
 
@@ -25,23 +21,34 @@ class LatentFactorRelationalModel(object):
 
         self.priors = priors
 
+        self.data_dist = DataDistribution()
+
+        self.joint_dist = JointDistribution(feat_alloc_prior, priors, symmetric=symmetric)
+
+        self.symmetric = symmetric
+
     @property
     def log_p(self):
-        data = self.data
+        return self.joint_dist.log_p(self.data, self.params)
 
-        params = self.params
+    def get_params_from_data(self):
+        N = self.data.shape[1]
 
-        log_p = 0
+        Z = self.feat_alloc_prior.rvs(N)
 
-        log_p += self.data_dist.log_p(data, params)
+        K = Z.shape[1]
 
-        log_p += self.feat_alloc_prior.log_p(params.Z)
+        if K == 0:
+            V = np.zeros((K, K))
 
-        log_p += np.sum(scipy.stats.norm.logpdf(
-            np.squeeze(params.V[np.triu_indices(params.K)]), 0, 1 / np.sqrt(params.tau)
-        ))
+        else:
+            V = np.random.normal(0, 1, size=(K, K))
 
-        return log_p
+            V = np.triu(V)
+
+            V = V + V.T - np.diag(np.diag(V))
+
+        return Parameters(1, V, Z)
 
     def predict(self, method='max'):
         V = self.params.V
@@ -67,75 +74,102 @@ class LatentFactorRelationalModel(object):
 
         return X
 
-    def update(self):
-        self.params = self.feat_alloc_updater.update(
-            self.data, self.data_dist, self.feat_alloc_prior, self.params
-        )
-
-        self.params = update_V(self.data, self.params)
-
-#         self.params = update_tau(self.params, self.priors)
-
 
 #=========================================================================
 # Updates
 #=========================================================================
-def update_alpha(params, priors):
-    a = priors.alpha[0] + params.K
+class LatentFactorRelationalModelUpdater(object):
+    def __init__(self, feat_alloc_updater):
+        self.feat_alloc_updater = feat_alloc_updater
 
-    b = priors.alpha[1] + np.sum(1 / np.arange(1, params.N + 1))
+    def update(self, model):
+        self.feat_alloc_updater.update(model)
 
-    params.alpha = np.random.gamma(a, 1 / b)
+        model.params = update_V(model.data, model.params, symmetric=model.symmetric)
 
-    return params
+        model.params = update_tau(model.params, model.priors, symmetric=model.symmetric)
+
+        model.feat_alloc_prior.update(model.params.Z)
 
 
-def update_tau(params, priors):
-    a = priors.tau[0] + 0.5 * (0.5 * params.K * (params.K + 1))
+def update_tau(params, priors, symmetric=True):
+    if symmetric:
+        a = priors.tau[0] + 0.5 * (0.5 * params.K * (params.K + 1))
 
-    b = priors.tau[1] + 0.5 * np.sum(np.square(np.triu(params.V)))
+        b = priors.tau[1] + 0.5 * np.sum(np.square(np.triu(params.V)))
+
+    else:
+        a = priors.tau[0] + 0.5 * params.K ** 2
+
+        b = priors.tau[1] + 0.5 * np.sum(np.square(params.V.flatten()))
 
     params.tau = np.random.gamma(a, 1 / b)
 
     return params
 
 
-def update_V(data, params, proposal_precision=10):
+def update_V(data, params, proposal_precision=1, symmetric=True):
     density = DataDistribution()
 
     proposal_std = 1 / np.sqrt(proposal_precision)
 
-    for i in range(params.K):
-        for j in range(i, params.K):
-            v_old = params.V[i, j]
+    if symmetric:
+        for i in np.random.permutation(params.K):
+            for j in np.random.permutation(np.arange(i, params.K)):
+                v_old = params.V[i, j]
 
-            v_new = np.random.normal(v_old, proposal_std)
+                v_new = np.random.normal(v_old, proposal_std)
 
-            params.V[i, j] = v_old
+                params.V[i, j] = v_old
 
-            params.V[j, i] = v_old
+                params.V[j, i] = v_old
 
-            log_p_old = density.log_p(data, params)
+                log_p_old = density.log_p(data, params)
 
-            log_q_old = scipy.stats.norm.logpdf(v_old, v_new, proposal_std)
+                log_q_old = scipy.stats.norm.logpdf(v_old, v_new, proposal_std)
 
-            params.V[i, j] = v_new
-
-            params.V[j, i] = v_new
-
-            log_p_new = density.log_p(data, params)
-
-            log_q_new = scipy.stats.norm.logpdf(v_new, v_new, proposal_std)
-
-            if do_metropolis_hastings_accept_reject(log_p_new, log_p_old, log_q_new, log_q_old):
                 params.V[i, j] = v_new
 
                 params.V[j, i] = v_new
 
-            else:
+                log_p_new = density.log_p(data, params)
+
+                log_q_new = scipy.stats.norm.logpdf(v_new, v_new, proposal_std)
+
+                if do_metropolis_hastings_accept_reject(log_p_new, log_p_old, log_q_new, log_q_old):
+                    params.V[i, j] = v_new
+
+                    params.V[j, i] = v_new
+
+                else:
+                    params.V[i, j] = v_old
+
+                    params.V[j, i] = v_old
+
+    else:
+        for i in np.random.permutation(params.K):
+            for j in np.random.permutation(params.K):
+                v_old = params.V[i, j]
+
+                v_new = np.random.normal(v_old, proposal_std)
+
                 params.V[i, j] = v_old
 
-                params.V[j, i] = v_old
+                log_p_old = density.log_p(data, params)
+
+                log_q_old = scipy.stats.norm.logpdf(v_old, v_new, proposal_std)
+
+                params.V[i, j] = v_new
+
+                log_p_new = density.log_p(data, params)
+
+                log_q_new = scipy.stats.norm.logpdf(v_new, v_new, proposal_std)
+
+                if do_metropolis_hastings_accept_reject(log_p_new, log_p_old, log_q_new, log_q_old):
+                    params.V[i, j] = v_new
+
+                else:
+                    params.V[i, j] = v_old
 
     return params
 
@@ -143,7 +177,6 @@ def update_V(data, params, proposal_precision=10):
 #=========================================================================
 # Densities and proposals
 #=========================================================================
-@numba.jitclass([])
 class DataDistribution(object):
     def __init__(self):
         pass
@@ -157,24 +190,62 @@ class DataDistribution(object):
         return log_p
 
     def log_p_row(self, data, params, row_idx):
-        V = params.V
-        Z = params.Z.astype(np.float64)
-        X = data
+        return _log_p_row(params.V, data, params.Z.astype(np.float64), row_idx)
 
-        log_p = _get_log_p_sigmoid(row_idx, row_idx, X, V, Z)
 
-        for i in range(params.N):
-            if i == row_idx:
-                continue
+class JointDistribution(object):
+    def __init__(self, feat_alloc_prior, priors, symmetric=True):
+        self.data_dist = DataDistribution()
 
-            log_p += _get_log_p_sigmoid(i, row_idx, X, V, Z)
+        self.feat_alloc_prior = feat_alloc_prior
 
-            log_p += _get_log_p_sigmoid(row_idx, i, X, V, Z)
+        self.priors = priors
+
+        self.symmetric = symmetric
+
+    def log_p(self, data, params):
+        log_p = 0
+
+        # Binary matrix prior
+        log_p += self.feat_alloc_prior.log_p(params.Z)
+
+        # Data
+        log_p += self.data_dist.log_p(data, params)
+
+        # Prior
+        if self.symmetric:
+            log_p += np.sum(scipy.stats.norm.logpdf(
+                np.squeeze(params.V[np.triu_indices(params.K)]), 0, 1 / np.sqrt(params.tau)
+            ))
+
+        else:
+            log_p += np.sum(scipy.stats.norm.logpdf(
+                params.V.flatten(), 0, 1 / np.sqrt(params.tau)
+            ))
 
         return log_p
 
 
-@numba.njit
+@numba.njit(cache=True)
+def _log_p_row(V, X, Z, row_idx):
+    N = X.shape[0]
+
+    log_p = 0
+
+    log_p += _get_log_p_sigmoid(row_idx, row_idx, X, V, Z)
+
+    for i in range(N):
+        if i == row_idx:
+            continue
+
+        log_p += _get_log_p_sigmoid(i, row_idx, X, V, Z)
+
+        log_p += _get_log_p_sigmoid(row_idx, i, X, V, Z)
+
+    return log_p
+
+
+@numba.njit(cache=True)
 def _get_log_p_sigmoid(i, j, X, V, Z):
     if np.isnan(X[i, j]):
         log_p = 0
@@ -193,78 +264,11 @@ def _get_log_p_sigmoid(i, j, X, V, Z):
     return log_p
 
 
-class Proposal(object):
-    def __init__(self, row_idx):
-        self.row_idx = row_idx
-
-    def rvs(self, data, params, num_singletons):
-        m = np.sum(params.Z, axis=0)
-
-        m -= params.Z[self.row_idx]
-
-        non_singletons_idxs = np.atleast_1d(np.squeeze(np.where(m > 0)))
-
-        num_non_singleton = len(non_singletons_idxs)
-
-        K_new = num_non_singleton + num_singletons
-
-        Z_new = np.zeros((params.N, K_new), dtype=np.int64)
-
-        Z_new[:, :num_non_singleton] = params.Z[:, non_singletons_idxs]
-
-        Z_new[self.row_idx, num_non_singleton:] = 1
-
-        V_new = np.zeros((K_new, K_new))
-
-        for i in range(num_non_singleton):
-            for j in range(num_non_singleton):
-                V_new[i, j] = params.V[non_singletons_idxs[i], non_singletons_idxs[j]]
-
-        for i in range(num_non_singleton, K_new):
-            for j in range(i, K_new):
-                V_new[i, j] = np.random.normal(0, 1 / np.sqrt(params.tau))
-
-                V_new[j, i] = V_new[i, j]
-
-        return Parameters(
-            params.alpha,
-            params.tau,
-            V_new,
-            Z_new
-        )
-
-
 #=========================================================================
 # Container classes
 #=========================================================================
-def get_params_from_data(data, feat_alloc_prior):
-    N = data.shape[1]
-
-    Z = feat_alloc_prior.rvs(N)
-
-    K = Z.shape[1]
-
-    if K == 0:
-        V = np.zeros((K, K))
-
-    else:
-        V = np.random.normal(0, 1, size=(K, K))
-        V = np.triu(V)
-        V = V + V.T - np.diag(np.diag(V))
-
-    return Parameters(1, 1, V, Z)
-
-
-@numba.jitclass([
-    ('alpha', numba.float64),
-    ('tau', numba.float64),
-    ('V', numba.float64[:, :]),
-    ('Z', numba.int64[:, :])
-])
 class Parameters(object):
-    def __init__(self, alpha, tau, V, Z):
-        self.alpha = alpha
-
+    def __init__(self, tau, V, Z):
         self.tau = tau
 
         self.V = V
@@ -281,7 +285,6 @@ class Parameters(object):
 
     def copy(self):
         return Parameters(
-            self.alpha,
             self.tau,
             self.V.copy(),
             self.Z.copy()
@@ -289,20 +292,85 @@ class Parameters(object):
 
 
 class Priors(object):
-    def __init__(self, alpha=None, tau=None, Z=None):
-        if alpha is None:
-            alpha = np.ones(2)
-
-            alpha = np.array([1, 10])
-
-        self.alpha = alpha
-
+    def __init__(self, tau=None):
         if tau is None:
             tau = np.ones(2)
 
         self.tau = tau
 
-        if Z is None:
-            Z = np.ones(2)
 
-        self.Z = Z
+#=========================================================================
+# Singletons updaters
+#=========================================================================
+class PriorSingletonsUpdater(object):
+    def update_row(self, model, row_idx):
+        k_old = len(get_singleton_idxs(model.params.Z, row_idx))
+
+        k_new = model.feat_alloc_prior.sample_num_singletons(model.params.Z)
+
+        if (k_new == 0) and (k_old == 0):
+            return model.params
+
+        non_singleton_idxs = get_non_singleton_idxs(model.params.Z, row_idx)
+
+        num_non_singletons = len(non_singleton_idxs)
+
+        K_new = len(non_singleton_idxs) + k_new
+
+        Z_new = np.zeros((model.params.N, K_new), dtype=np.int64)
+
+        Z_new[:, :num_non_singletons] = model.params.Z[:, non_singleton_idxs]
+
+        Z_new[row_idx, num_non_singletons:] = 1
+
+        V_new = np.zeros((K_new, K_new))
+
+        for i in range(num_non_singletons):
+            for j in range(num_non_singletons):
+                V_new[i, j] = model.params.V[non_singleton_idxs[i], non_singleton_idxs[j]]
+
+        if model.symmetric:
+            for i in range(num_non_singletons, K_new):
+                for j in range(i, K_new):
+                    V_new[i, j] = np.random.normal(0, 1 / np.sqrt(model.params.tau))
+
+                    V_new[j, i] = V_new[i, j]
+
+        else:
+            for i in range(num_non_singletons, K_new):
+                for j in range(num_non_singletons, K_new):
+                    V_new[j, i] = np.random.normal(0, 1 / np.sqrt(model.params.tau))
+
+        params_new = Parameters(model.params.tau, V_new, Z_new)
+
+        log_p_new = model.data_dist.log_p_row(model.data, params_new, row_idx)
+
+        log_p_old = model.data_dist.log_p_row(model.data, model.params, row_idx)
+
+        if do_metropolis_hastings_accept_reject(log_p_new, log_p_old, 0, 0):
+            params = params_new
+
+        else:
+            params = model.params
+
+        return params
+
+
+def get_column_counts(Z, row_idx):
+    m = np.sum(Z, axis=0)
+
+    m -= Z[row_idx]
+
+    return m
+
+
+def get_non_singleton_idxs(Z, row_idx):
+    m = get_column_counts(Z, row_idx)
+
+    return np.atleast_1d(np.squeeze(np.where(m > 0)))
+
+
+def get_singleton_idxs(Z, row_idx):
+    m = get_column_counts(Z, row_idx)
+
+    return np.atleast_1d(np.squeeze(np.where(m == 0)))
