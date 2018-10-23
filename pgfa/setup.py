1e3ad99f139bb279aae36b6a08cc43e1ccee2bce
Andrew Roth <andrewjlroth@gmail.com>
2018-10-23 12:44:28 -0700

    feature: setup file

---
+++ pgfa/setup.py
@@ -0,0 +1,7 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='pgfa',
+    description='Particle Gibbs row sampler for feature allocation models',
+    packages=find_packages(),
+)
