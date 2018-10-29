ec30c669c4972825a9e53f5ee2e42d16805221d4
Andrew Roth <andrewjlroth@gmail.com>
2018-10-29 13:59:58 -0700

    tests: updating mocks to new interface

--- pgfa/tests/mocks.py
+++ pgfa/tests/mocks.py
@@ -21,13 +21,13 @@ class MockFeatureAllocationPrior(object):
     def __init__(self, p=1e-4):
         self.p = p
 
-    def get_feature_probs(self, row_idx, Z):
-        K = Z.shape[1]
+    def get_feature_probs(self, params, row_idx):
+        K = params.Z.shape[1]
 
         return self.p * np.ones(K)
 
-    def get_update_cols(self, row_idx, Z):
-        K = Z.shape[1]
+    def get_update_cols(self, params, row_idx):
+        K = params.Z.shape[1]
 
         cols = np.arange(K)
 
@@ -35,13 +35,17 @@ class MockFeatureAllocationPrior(object):
 
         return cols
 
-    def log_p(self, Z):
-        return np.sum(Z) * np.log(self.p) + np.sum(1 - Z) * np.log(1 - self.p)
+    def log_p(self, params):
+        return np.sum(params.Z * np.log(self.p)) + np.sum((1 - params.Z) * np.log(1 - self.p))
 
 
 class MockParams(object):
-    def __init__(self, K, N):
+    def __init__(self, alpha, K, N):
+        self.alpha = alpha
+
         self.Z = np.random.randint(0, 2, size=(N, K))
+
+        self.Z = np.ones((N, K))
 
     @property
     def K(self):
