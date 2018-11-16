d10cf31068e9baea8fb6a8ae15a04f80e5c0f398
Andrew Roth <andrewjlroth@gmail.com>
2018-11-15 18:12:17 -0800

    minor: relative import

--- pgfa/updates/__init__.py
+++ pgfa/updates/__init__.py
@@ -1,5 +1,5 @@
 from .gibbs import GibbsUpdater
 from .metropolis_hastings import MetropolisHastingsUpdater
 from .mixture import GibbsMixtureUpdater
-from pgfa.updates.particle_gibbs import ParticleGibbsUpdater
+from .particle_gibbs import ParticleGibbsUpdater
 from .row_gibbs import RowGibbsUpdater
