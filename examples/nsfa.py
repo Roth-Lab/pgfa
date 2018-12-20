6a39d6c09c26766d2058363c1b5af0d6e6d8212f
Andrew Roth <andrewjlroth@gmail.com>
2018-12-19 17:15:43 -0800

    minor: cleaning up nsfa example

--- examples/nsfa.py
+++ examples/nsfa.py
@@ -1,6 +1,8 @@
+import numba
 import numpy as np
+import scipy.stats
 
-from pgfa.utils import get_b_cubed_score
+from pgfa.utils import get_b_cubed_score, Timer
 
 import pgfa.feature_allocation_distributions
 import pgfa.models.nsfa
@@ -10,175 +12,158 @@ import pgfa.updates
 def main():
     np.random.seed(0)
 
-    ibp = True
-    num_iters = int(1e5)
-    D = 10
-    K = 20
-    N_train = 100
-    N_test = 100
+    ibp = False
+    time = 1000
+    updater = 'rg'
+    updater_mixed = False
+    D = 100
+    K = 5
+    N = 1000
 
-    feat_alloc_dist = pgfa.feature_allocation_distributions.get_feature_allocation_distribution(K)
+    data, data_true, params = simulate_data(D, N, K=K, alpha=5, prop_missing=0, tau_v=0.25, tau_x=25)
 
-    params_train = get_test_params(feat_alloc_dist, N_train, D, alpha=K, gamma=0.25, S=25, seed=0)
-
-    params_test = get_test_params(feat_alloc_dist, N_test, D, params=params_train, seed=1)
-
-    data_train = get_data(params_train)
-
-    data_test = get_data(params_test)
-
-    if ibp:
-        singletons_updater = pgfa.models.nsfa.PriorSingletonsUpdater()
-
-        feat_alloc_dist = pgfa.feature_allocation_distributions.get_feature_allocation_distribution(None)
-
-    else:
-        singletons_updater = None
-
-        feat_alloc_dist = pgfa.feature_allocation_distributions.get_feature_allocation_distribution(K)
-
-    feat_alloc_updater = pgfa.updates.GibbsMixtureUpdater(
-        pgfa.updates.ParticleGibbsUpdater(
-            annealed=True, num_particles=20, singletons_updater=singletons_updater
-        )
+    model_updater = get_model_updater(
+        feat_alloc_updater_type=updater, ibp=ibp, mixed_updates=updater_mixed
     )
 
-#     feat_alloc_updater = pgfa.updates.GibbsUpdater(singletons_updater=singletons_updater)
+    model = get_model(data, ibp=ibp, K=K)
 
-    model_updater = pgfa.models.nsfa.ModelUpdater(feat_alloc_updater)
-
-    model = pgfa.models.nsfa.Model(data_train, feat_alloc_dist)
-
-    params_orig = model.params.copy()
-
-    model.params = params_train.copy()
-
-    log_p_true = model.log_p
-
-    model.params = params_orig
-
-    print(np.sum(params_train.Z, axis=0))
+    print(np.sum(params.Z, axis=0))
 
     print('@' * 100)
 
-    for i in range(num_iters):
+    old_params = model.params.copy()
+
+    model.params = params.copy()
+
+    log_p_true = model.log_p
+
+    model.params = old_params.copy()
+
+    print(log_p_true)
+
+    timer = Timer()
+
+    i = 0
+
+    while timer.elapsed < time:
         if i % 100 == 0:
             print(
                 i,
-                model.params.Z.shape[1],
+                model.params.K,
                 (model.log_p - log_p_true) / abs(log_p_true),
-                model.log_predictive_pdf(data_test),
-                model.rmse,
-                model.params.gamma
+                pgfa.models.nsfa.log_held_out_pdf(data_true, np.isnan(data), model.params),
+                pgfa.models.nsfa.rmse(data, model.params)
             )
 
-            if model.params.K > 0:
-                try:
-                    print(get_b_cubed_score(params_train.Z, model.params.Z))
-                except:
-                    pass
+            print(get_b_cubed_score(params.Z, model.params.Z))
+
+            print(
+                model.data_dist.log_p(model.data, model.params),
+                model.feat_alloc_dist.log_p(model.params),
+                model.params_dist.log_p(model.params)
+            )
 
             print(np.sum(model.params.Z, axis=0))
 
             print('#' * 100)
 
-        model_updater.update(model, param_updates=10)
+        timer.start()
+
+        model_updater.update(model)
+
+        timer.stop()
+
+        i += 1
 
 
-def get_data(params):
-    data = params.W @ params.F
-
-    data += np.random.multivariate_normal(np.zeros(params.D), np.diag(1 / params.S), size=params.N).T
-
-    return data
-
-
-def get_test_params(feat_alloc_dist, num_data_points, num_observed_dims, alpha=1, gamma=1, S=1, params=None, seed=None):
-    if seed is not None:
-        np.random.seed(seed)
-
-    D = num_observed_dims
-    N = num_data_points
-
-    if params is None:
-        Z = feat_alloc_dist.rvs(alpha, D)
-
-        K = Z.shape[1]
-
-        S = S * np.ones(D)
-
-        V = np.random.multivariate_normal(np.zeros(K), (1 / gamma) * np.eye(K), size=D)
-
-        F = np.random.normal(0, 1, size=(K, N))
+def get_model(data, ibp=False, K=None):
+    if ibp:
+        feat_alloc_dist = pgfa.feature_allocation_distributions.IndianBuffetProcessDistribution()
 
     else:
-        alpha = params.alpha
+        feat_alloc_dist = pgfa.feature_allocation_distributions.BetaBernoulliFeatureAllocationDistribution(K)
 
-        gamma = params.gamma
+    return pgfa.models.nsfa.Model(data, feat_alloc_dist)
 
-        Z = params.Z.copy()
 
-        K = Z.shape[1]
+def get_model_updater(feat_alloc_updater_type='g', ibp=True, mixed_updates=False):
+    if ibp:
+        singletons_updater = pgfa.models.nsfa.CollapsedSingletonsUpdater()
 
-        S = params.S.copy()
+    else:
+        singletons_updater = None
 
-        V = params.V.copy()
+    if feat_alloc_updater_type == 'g':
+        feat_alloc_updater = pgfa.updates.GibbsUpdater(singletons_updater=singletons_updater)
 
-        F = np.random.normal(0, 1, size=(K, N))
+    elif feat_alloc_updater_type == 'pg':
+        feat_alloc_updater = pgfa.updates.ParticleGibbsUpdater(
+            annealed=False, num_particles=20, singletons_updater=singletons_updater
+        )
 
-    return pgfa.models.nsfa.Parameters(
-        alpha, np.ones(2), gamma, np.ones(2), F, S, np.ones(2), V, Z
+    elif feat_alloc_updater_type == 'pga':
+        feat_alloc_updater = pgfa.updates.ParticleGibbsUpdater(
+            annealed=True, num_particles=5, singletons_updater=singletons_updater
+        )
+
+    elif feat_alloc_updater_type == 'rg':
+        feat_alloc_updater = pgfa.updates.RowGibbsUpdater(singletons_updater=singletons_updater)
+
+    if mixed_updates:
+        feat_alloc_updater = pgfa.updates.GibbsMixtureUpdater(feat_alloc_updater)
+
+    return pgfa.models.nsfa.ModelUpdater(feat_alloc_updater)
+
+
+def set_seed(seed):
+    np.random.seed(seed)
+
+    set_numba_seed(seed)
+
+
+@numba.jit
+def set_numba_seed(seed):
+    np.random.seed(seed)
+
+
+def simulate_data(D, N, K=None, alpha=1, prop_missing=0, tau_v=1, tau_x=1):
+    feat_alloc_dist = pgfa.feature_allocation_distributions.get_feature_allocation_distribution(K=K)
+
+    Z = feat_alloc_dist.rvs(alpha, D)
+
+    Z[:, 4:] = 0
+
+    K = Z.shape[1]
+
+    tau_v = tau_v * np.ones(K)
+
+    tau_x = tau_x * np.ones(D)
+
+    V = np.zeros((D, K))
+
+    F = np.zeros((K, N))
+
+    if K > 0:
+        for k in range(K):
+            V[:, k] = scipy.stats.multivariate_normal.rvs(np.zeros(D), (1 / tau_v[k]) * np.eye(D))
+
+            F[k] = scipy.stats.norm.rvs(0, 1, size=N)
+
+    params = pgfa.models.nsfa.Parameters(
+        alpha, np.ones(2), tau_v, np.ones(2), tau_x, np.ones(2), F, V, Z
     )
 
+    data = params.Y
 
-def get_min_error(params_pred, params_true):
-    import itertools
+    data += np.random.multivariate_normal(np.zeros(params.D), np.diag(1 / params.tau_x), size=params.N)
 
-    W_pred = params_pred.F.T
+    data_true = data.copy()
 
-    W_true = params_true.F.T
+    data[np.random.random(data.shape) <= prop_missing] = np.nan
 
-    K_pred = W_pred.shape[1]
-
-    K_true = W_true.shape[1]
-
-    min_error = float('inf')
-
-    for perm in itertools.permutations(range(K_pred)):
-        error = np.sqrt(np.mean((W_pred[:, perm[:K_true]] - W_true)**2))
-
-        if error < min_error:
-            min_error = error
-
-    return min_error
+    return data, data_true, params
 
 
 if __name__ == '__main__':
-    #     import line_profiler
-    #
-    #     fast = True
-    #
-    #     if fast:
-    #         import pgfa.updates.particle_gibbs_target_ratio
-    #
-    #         profiler = line_profiler.LineProfiler(
-    #             pgfa.models.nsfa.ModelUpdater.update,
-    #             pgfa.updates.particle_gibbs_target_ratio.do_particle_gibbs_update,
-    #             pgfa.updates.particle_gibbs_target_ratio._propose,
-    #         )
-    #
-    #     else:
-    #         import pgfa.updates.particle_gibbs
-    #
-    #         profiler = line_profiler.LineProfiler(
-    #             pgfa.models.nsfa.ModelUpdater.update,
-    #             pgfa.updates.particle_gibbs.do_particle_gibbs_update,
-    #             pgfa.updates.particle_gibbs._propose,
-    #             pgfa.updates.particle_gibbs._log_target_pdf_annealed
-    #         )
-    #
-    #     profiler.run("main()")
-    #
-    #     profiler.print_stats()
-
     main()
