4b165fc4a50cb66d5ccc8aa1a009a36411a19671
Andrew Roth <andrewjlroth@gmail.com>
2019-02-22 16:54:47 -0800

    refactor: cleaning up dpf code

--- pgfa/updates/discrete_particle_filter.py
+++ pgfa/updates/discrete_particle_filter.py
@@ -1,12 +1,16 @@
+import numba
 import numpy as np
 
-from pgfa.math_utils import discrete_rvs, log_normalize, log_sum_exp
-
+from pgfa.data_structures import Particle, ParticleSwarm
+from pgfa.math_utils import conditional_stratified_resampling, log_sum_exp, stratified_resampling
 from pgfa.updates.base import FeatureAllocationMatrixUpdater
 
 
 class DicreteParticleFilterUpdater(FeatureAllocationMatrixUpdater):
-    def __init__(self, max_particles=10, singletons_updater=None):
+
+    def __init__(self, annealed=False, max_particles=10, singletons_updater=None):
+        self.annealed = annealed
+        
         self.max_particles = max_particles
 
         self.singletons_updater = singletons_updater
@@ -14,82 +18,83 @@ class DicreteParticleFilterUpdater(FeatureAllocationMatrixUpdater):
     def update_row(self, cols, data, dist, feat_probs, params, row_idx):
         T = len(cols)
 
-        conditional_path = params.Z[row_idx, cols]
+        conditional_path = params.Z[row_idx, cols].copy()
+        
+        log_feat_probs = np.row_stack([np.log1p(-feat_probs), np.log(feat_probs)])
 
-        particles = [None]
+        swarm = ParticleSwarm()
+        
+        swarm.add_particle(0, None)
+        
+        for t in range(T):            
+            if swarm.num_particles > self.max_particles:
+                swarm = self._resample(swarm)
 
-        for t in range(T):
-            if t > 0:
-                assert np.all(particles[0].genealogy == conditional_path[:t])
+            new_swarm = ParticleSwarm()
 
-            if len(particles) > self.max_particles:
-                particles = self._resample(particles)
+            annealing_factor = self._get_annealing_factor(t, T)
+            
+            col = cols[t]
+            
+            states = [conditional_path[t], 1 - conditional_path[t]]
 
-            new_particles = []
-
-            for i, p in enumerate(particles):
-                if i == 0:
-                    new_particles.append(
-                        self._get_new_particle(
-                            cols[:t + 1], data, dist, feat_probs, params, p, row_idx, conditional_path[t]
-                        )
+            for log_W, parent_particle in zip(swarm.log_weights, swarm.particles):
+                if parent_particle is not None:
+                    params.Z[row_idx, cols[:t]] = parent_particle.path
+          
+                for s in states:
+                    particle = self._get_new_particle(
+                        annealing_factor, col, data, dist, log_feat_probs, params, parent_particle, row_idx, s
                     )
+                    
+                    new_swarm.add_particle(log_W + particle.log_w, particle)
 
-                for s in [0, 1]:
-                    if (i == 0) and (s == conditional_path[t]):
-                        continue
+            swarm = new_swarm
+        
+        assert np.all(swarm[0].path == conditional_path)
 
-                    new_particles.append(
-                        self._get_new_particle(
-                            cols[:t + 1], data, dist, feat_probs, params, p, row_idx, s
-                        )
-                    )
-
-            particles = new_particles
-
-        W = np.exp(log_normalize(np.array([p.log_w for p in particles])))
-
-        idx = discrete_rvs(W)
-
-        params.Z[row_idx, cols] = particles[idx].genealogy
+        params.Z[row_idx, cols] = swarm.sample().path
 
         return params
 
-    def _get_new_particle(self, cols, data, dist, feat_probs, params, parent, row_idx, value):
+    def _get_annealing_factor(self, t, T):
+        if self.annealed:
+            annealing_factor = (t + 1) / T
+        
+        else:
+            annealing_factor = 1
+            
+        return annealing_factor
+
+    def _get_new_particle(self, annealing_factor, col, data, dist, log_feat_probs, params, parent, row_idx, value):
         if parent is None:
-            genealogy = []
-
-            log_w = 0
-
             parent_log_p = 0
+            
+            parent_path = []
 
         else:
-            genealogy = parent.genealogy
-
-            log_w = parent.log_w
-
             parent_log_p = parent.log_p
+            
+            parent_path = parent.path
 
-        genealogy.append(value)
+        params.Z[row_idx, col] = value
+        
+        prior = log_feat_probs[value, col]
 
-        genealogy = np.array(genealogy, dtype=np.int)
+        log_p = annealing_factor * dist.log_p_row(data, params, row_idx)
+        
+        log_w = prior + log_p - parent_log_p
 
-        params.Z[row_idx, cols] = genealogy
+        return Particle(log_p, log_w, parent, parent_path + [value])
 
-        log_p = np.sum(genealogy * np.log(feat_probs[cols]) + (1 - genealogy) * np.log(1 - feat_probs[cols]))
+    def _resample(self, swarm):
+        log_W = swarm.log_weights
 
-        log_p += dist.log_p_row(data, params, row_idx)
-
-        return Particle(log_p, log_w + log_p - parent_log_p, parent, value)
-
-    def _resample(self, particles):
-        log_W = log_normalize(np.array([p.log_w for p in particles]))
-
-        keep_idxs, resample_idxs, log_C = self._split_particles(particles)
+        keep_idxs, resample_idxs, log_C = _split_particles(log_W, self.max_particles)
 
         num_resampled_particles = self.max_particles - len(keep_idxs)
 
-        resample_particles = [particles[i] for i in resample_idxs]
+        resample_particles = [swarm.particles[i] for i in resample_idxs]
 
         if 0 in keep_idxs:
             resample_idxs_sub = self._stratified_resample(num_resampled_particles, resample_particles)
@@ -102,115 +107,64 @@ class DicreteParticleFilterUpdater(FeatureAllocationMatrixUpdater):
         resample_idxs = [resample_idxs[i] for i in resample_idxs_sub]
 
         idxs = sorted(keep_idxs + resample_idxs)
+        
+        try:
+            assert idxs[0] == 0
+        
+        except AssertionError:
+            print(resample_idxs)
+            print(keep_idxs)
 
-        assert idxs[0] == 0
-
-        new_particles = []
+        new_swarm = ParticleSwarm()
 
         for i in idxs:
-            p = particles[i]
-
             if i in keep_idxs:
-                new_particles.append(Particle(p.log_p, log_W[i], p.parent, p.value))
+                new_swarm.add_particle(log_W[i], swarm[i])
 
             else:
-                new_particles.append(Particle(p.log_p, -log_C, p.parent, p.value))
+                new_swarm.add_particle(-log_C, swarm[i])
 
-        return new_particles
-
-    def _split_particles(self, particles):
-        N = self.max_particles
-
-        log_W = log_normalize(np.array([p.log_w for p in particles]))
-
-        for idx in np.argsort(log_W):
-            log_kappa = log_W[idx]
-
-            total = log_W - log_kappa
-
-            total[total > 0] = 0
-
-            total = log_sum_exp(total)
-
-            if total <= np.log(N):
-                break
-
-        A = np.sum(log_W > log_kappa)
-
-        B = log_sum_exp(log_W[log_W <= log_kappa])
-
-        log_C = np.log(N - A) - B
-
-        kept, resamp = [], []
-
-        for i in range(len(log_W)):
-            if log_W[i] > -log_C:
-                kept.append(i)
-
-            else:
-                resamp.append(i)
-
-        return kept, resamp, log_C
+        return new_swarm
 
     def _conditional_stratified_resample(self, num_resampled, particles):
-        W = np.exp(log_normalize(np.array([p.log_w for p in particles])))
-
-        U = np.random.uniform(0, W[0])
-
-        positions = (U - np.floor(num_resampled * U) / num_resampled) + np.arange(num_resampled) / num_resampled
-
-        indexes = []
-
-        cumulative_sum = np.cumsum(W)
-
-        i, j = 0, 0
-
-        while i < num_resampled:
-            if positions[i] < cumulative_sum[j]:
-                indexes.append(j)
-
-                i += 1
-
-            else:
-                j += 1
-
-        return indexes
+        log_w = np.array([p.log_w for p in particles])
+        
+        return conditional_stratified_resampling(log_w, num_resampled)
 
     def _stratified_resample(self, num_resampled, particles):
-        W = np.exp(log_normalize(np.array([p.log_w for p in particles])))
-
-        positions = (np.random.random() + np.arange(num_resampled)) / num_resampled
-
-        indexes = []
-
-        cumulative_sum = np.cumsum(W)
-
-        i, j = 0, 0
-
-        while i < num_resampled:
-            if positions[i] < cumulative_sum[j]:
-                indexes.append(j)
-
-                i += 1
-
-            else:
-                j += 1
-
-        return indexes
+        log_w = np.array([p.log_w for p in particles])
+        
+        return stratified_resampling(log_w, num_resampled)
 
 
-class Particle(object):
-    def __init__(self, log_p, log_w, parent, value):
-        self.log_p = log_p
-        self.log_w = log_w
-        self.parent = parent
-        self.value = value
+@numba.njit(cache=True)
+def _split_particles(log_W, N):
+    for idx in np.argsort(log_W):
+        log_kappa = log_W[idx]
 
-    @property
-    def genealogy(self):
-        genealogy = [self.value]
-        parent = self.parent
-        while parent is not None:
-            genealogy.append(parent.value)
-            parent = parent.parent
-        return list(reversed(genealogy))
+        log_ratio = log_W - log_kappa
+
+        log_ratio[log_ratio > 0] = 0
+
+        total = log_sum_exp(log_ratio)
+
+        if total <= np.log(N):
+            break
+
+    A = np.sum(log_W > log_kappa)
+
+    B = log_sum_exp(log_W[log_W <= log_kappa])
+
+    log_C = np.log(N - A) - B
+
+    kept, resamp = [], []
+
+    for i in range(len(log_W)):
+        if log_W[i] > -log_C:
+            kept.append(i)
+
+        else:
+            resamp.append(i)
+
+    return kept, resamp, log_C
+
