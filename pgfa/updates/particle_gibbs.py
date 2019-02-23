d8cb305498918771642f571c59589e5a0c08f26a
Andrew Roth <andrewjlroth@gmail.com>
2019-02-22 16:54:33 -0800

    refactor: cleaning up pg code

--- pgfa/updates/particle_gibbs.py
+++ pgfa/updates/particle_gibbs.py
@@ -1,10 +1,7 @@
-from collections import namedtuple
-
-import numba
 import numpy as np
 
-from pgfa.math_utils import discrete_rvs, log_normalize, log_sum_exp
-
+from pgfa.data_structures import Particle, ParticleSwarm
+from pgfa.math_utils import conditional_stratified_resampling, discrete_rvs, log_sum_exp
 from pgfa.updates.base import FeatureAllocationMatrixUpdater
 
 
@@ -18,218 +15,102 @@ class ParticleGibbsUpdater(FeatureAllocationMatrixUpdater):
         self.resample_threshold = resample_threshold
 
         self.singletons_updater = singletons_updater
-
-    def update_row(self, cols, data, dist, feat_probs, params, row_idx):
-        return do_particle_gibbs_update(
-            cols,
-            data,
-            dist,
-            feat_probs,
-            params,
-            row_idx,
-            annealed=self.annealed,
-            num_particles=self.num_particles,
-            resample_threshold=self.resample_threshold
-        )
-
-
-Particle = namedtuple('Particle', ['log_p', 'log_w', 'parent', 'value'])
-
-
-def do_particle_gibbs_update(
-        cols,
-        data,
-        dist,
-        feat_probs,
-        params,
-        row_idx,
-        annealed=True,
-        num_particles=10,
-        resample_threshold=0.5):
-
-    T = len(cols)
-
-    log_W = np.zeros(num_particles)
-
-    particles = [None for _ in range(num_particles)]
-
-    z = params.Z[row_idx, cols]
-
-    Zs = np.zeros((num_particles, T))
-
-    for t in range(T):
-        log_W = log_normalize(log_W)
-
-        if t > 0:
-            log_W, particles, Zs = _resample(log_W, particles, Zs, conditional=True, threshold=resample_threshold)
-
-        for i in range(num_particles):
-            if i == 0:
-                idx = z[t]
-
-            else:
-                idx = -1
-
-            params.Z[row_idx, cols] = Zs[i]
-
-            particles[i] = _propose(
-                cols[t], data, dist, feat_probs, params, particles[i], row_idx, t + 1, T, annealed=annealed, idx=idx
-            )
-
-            Zs[i, t] = particles[i].value
-
-            log_W[i] = log_W[i] + particles[i].log_w
-
-    log_W = log_normalize(log_W)
-
-    W = np.exp(log_W)
-
-    idx = discrete_rvs(W)
-
-    params.Z[row_idx, cols] = Zs[idx]
-
-    return params
-
-
-@numba.njit(cache=True)
-def _propose_idx(col, feat_probs, log_p):
-    # Target
-    log_a = np.log(feat_probs[col])
-
-    log_b = np.log(1 - feat_probs[col])
-
-    log_target = np.zeros(2)
-
-    log_target[0] = log_p[0] + log_b
-
-    log_target[1] = log_p[1] + log_a
-
-    # Sample
-    log_norm = log_sum_exp(log_target)
-
-    p = np.exp(log_target - log_norm)
-
-    idx = discrete_rvs(p)
-
-    return idx, log_norm - log_p[0]
-
-
-@numba.njit(cache=True)
-def _propose_idx_annealed(col, feat_probs, log_p, t, T):
-    # Target
-    log_a = np.log(feat_probs[col])
-
-    log_b = np.log(1 - feat_probs[col])
-
-    log_target = np.zeros(2)
-
-    log_target[0] = (t / T) * log_p[0] + log_b
-
-    log_target[1] = (t / T) * log_p[1] + log_a
-
-    # Sample
-    log_norm = log_sum_exp(log_target)
-
-    p = np.exp(log_target - log_norm)
-
-    idx = discrete_rvs(p)
-
-    return idx, log_norm - ((t - 1) / T) * log_p[0]
-
-
-def _propose(col, data, dist, feat_probs, params, parent_particle, row_idx, t, T, annealed=False, idx=-1):
-    log_p = np.zeros(2)
-
-    # Feature off
-    if parent_particle is None:
-        params.Z[row_idx, col] = 0
-
-        log_p[0] = dist.log_p_row(data, params, row_idx)
-
-    else:
-        log_p[0] = parent_particle.log_p
-
-    # Feature on
-    params.Z[row_idx, col] = 1
-
-    log_p[1] = dist.log_p_row(data, params, row_idx)
-
-    if annealed:
-        prop_idx, log_w = _propose_idx_annealed(col, feat_probs, log_p, t, T)
-
-    else:
-        prop_idx, log_w = _propose_idx(col, feat_probs, log_p)
-
-    if idx == -1:
-        idx = prop_idx
     
-    idx = int(idx)
+    def update_row(self, cols, data, dist, feat_probs, params, row_idx):
+        T = len(cols)
 
-    return Particle(log_p[idx], log_w, parent_particle, idx)
+        conditional_path = params.Z[row_idx, cols].copy()
+        
+        log_feat_probs = np.row_stack([np.log1p(-feat_probs), np.log(feat_probs)])
 
+        swarm = ParticleSwarm()
+        
+        for _ in range(self.num_particles):
+            swarm.add_particle(0, None)
 
-def _get_ess(log_W):
-    W = np.exp(log_W)
+        for t in range(T):           
+            if t > 0:
+                particles = self._resample(swarm)
 
-    return 1 / np.sum(np.square(W))
+                assert np.all(particles[0].path == conditional_path[:t])
 
+            new_swarm = ParticleSwarm()
 
-def _resample(log_W, particles, Zs, conditional=True, threshold=0.5):
-    num_particles = len(particles)
+            annealing_factor = self._get_annealing_factor(t, T)
+            
+            col = cols[t]
 
-    if (_get_ess(log_W) / num_particles) <= threshold:
-        new_particles = []
+            for i, (parent_particle, log_W) in enumerate(zip(swarm.particles, swarm.log_weights)):
+                if parent_particle is not None:
+                    params.Z[row_idx, cols[:t]] = parent_particle.path
+                
+                if i == 0:
+                    value = conditional_path[t]
+                
+                else:
+                    value = None
+                
+                particle = self._get_new_particle(
+                    annealing_factor, col, data, dist, log_feat_probs, params, parent_particle, row_idx, value=value
+                )
+                
+                new_swarm.add_particle(log_W + particle.log_w, particle)
 
-        new_Zs = np.zeros(Zs.shape)
+            swarm = new_swarm
 
-        W = np.exp(log_W)
+        params.Z[row_idx, cols] = swarm.sample().path
 
-        W = W + 1e-10
+        return params
+    
+    def _get_annealing_factor(self, t, T):
+        if self.annealed:
+            annealing_factor = (t + 1) / T
+        
+        else:
+            annealing_factor = 1
+            
+        return annealing_factor
 
-        W = W / np.sum(W)
-
-        if conditional:
-            new_particles.append(particles[0])
-
-            new_Zs[0] = Zs[0]
-
-            multiplicity = np.random.multinomial(num_particles - 1, W)
-
-            idx = 1
+    def _get_new_particle(self, annealing_factor, col, data, dist, log_feat_probs, params, parent, row_idx, value=None):
+        if parent is None:
+            parent_log_p = 0
+            
+            parent_path = []
 
         else:
-            multiplicity = np.random.multinomial(num_particles, W)
+            parent_log_p = parent.log_p
+            
+            parent_path = parent.path
+        
+        log_q = np.zeros(2)
+        
+        log_p = np.zeros(2)
+        
+        for i in range(2):
+            params.Z[row_idx, col] = i
+    
+            log_p[i] = annealing_factor * dist.log_p_row(data, params, row_idx)
+            
+            log_q[i] = log_feat_probs[i, col] + log_p[i]
+        
+        log_norm = log_sum_exp(log_q)
+        
+        if value is None:
+            value = discrete_rvs(np.exp(log_q - log_norm)) 
+        
+        log_w = log_norm - parent_log_p
+        
+        return Particle(log_p[value], log_w, parent, parent_path + [value])
 
-            idx = 0
+    def _resample(self, swarm):
+        if swarm.relative_ess <= self.resample_threshold:
+            new_swarm = ParticleSwarm()
 
-        for k in range(num_particles):
-            for _ in range(multiplicity[k]):
-                new_particles.append(particles[k])
+            for idx in conditional_stratified_resampling(swarm.unnormalized_log_weights, self.num_particles):
+                new_swarm.add_particle(0, swarm.particles[idx])
 
-                new_Zs[idx] = Zs[k]
+        else:
+            new_swarm = swarm
 
-                idx += 1
+        return new_swarm
 
-        log_W = -np.log(num_particles) * np.ones(num_particles)
-
-        particles = new_particles
-
-        Zs = new_Zs
-
-    return log_W, particles, Zs
-
-
-def iter_particles(particle):
-    while particle is not None:
-        yield particle
-
-        particle = particle.parent
-
-
-def get_z(particle):
-    z = []
-
-    for p in iter_particles(particle):
-        z.append(p.value)
-
-    return z[::-1]
