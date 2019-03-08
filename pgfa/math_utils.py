bugfix: deterministically add conditional particle during stratified resampling to work around possible numerical issues

pgfa/math_utils.py
@@ -195,7 +195,7 @@ def cholesky_log_det(X):
     return 2 * np.sum(np.log(np.diag(X)))
 
 
-# @numba.njit(cache=True)
+@numba.njit(cache=True)
 def conditional_stratified_resampling(log_w, num_resampled):
     """ Perform conditional stratified resampling.
     
@@ -210,30 +210,28 @@ def conditional_stratified_resampling(log_w, num_resampled):
     -------
     indexes: (ndarray) Indexes of resampled values.
     """
-    log_W = log_normalize(log_w)
+    W = exp_normalize(log_w)
     
-    U = np.random.uniform(0, np.exp(log_W[0]))
-    
+    U = np.random.uniform(0, W[0])
+
     positions = (U - np.floor(num_resampled * U) / num_resampled) + np.arange(num_resampled) / num_resampled
-    
-    indexes = []
-    
-    log_cumulative_sum = np.logaddexp.accumulate(log_W)
-    
-    log_positions = np.log(positions)
-    
-    i, j = 0, 0
-    
+
+    indexes = [0]
+
+    cumulative_sum = np.cumsum(W)
+
+    i, j = 1, 0
+
     while i < num_resampled:
-        if log_positions[i] < log_cumulative_sum[j]:
+        if positions[i] < cumulative_sum[j]:
             indexes.append(j)
-            
+
             i += 1
-        
+
         else:
             j += 1
-    
-    return indexes
+
+    return indexes    
 
 
 @numba.njit(cache=True)