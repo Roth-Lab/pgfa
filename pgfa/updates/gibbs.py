934ce43ad863236d2371142f0109b92e9a22024d
Andrew Roth <andrewjlroth@gmail.com>
2019-03-26 09:37:15 -0700

    minor: gumbel trick sampling for Gibbs

--- pgfa/updates/gibbs.py
+++ pgfa/updates/gibbs.py
@@ -1,18 +1,19 @@
 import numpy as np
 
-from pgfa.math_utils import discrete_rvs, log_normalize
+from pgfa.math_utils import discrete_rvs_gumbel_trick
 
 from pgfa.updates.base import FeatureAllocationMatrixUpdater
 
 
 class GibbsUpdater(FeatureAllocationMatrixUpdater):
+
     def update_row(self, cols, data, dist, feat_probs, params, row_idx):
         log_p = np.zeros(2)
 
         for k in cols:
             params.Z[row_idx, k] = 0
 
-            log_p[0] = np.log(1 - feat_probs[k])
+            log_p[0] = np.log1p(-feat_probs[k])
             log_p[0] += dist.log_p_row(data, params, row_idx)
 
             params.Z[row_idx, k] = 1
@@ -20,8 +21,6 @@ class GibbsUpdater(FeatureAllocationMatrixUpdater):
             log_p[1] = np.log(feat_probs[k])
             log_p[1] += dist.log_p_row(data, params, row_idx)
 
-            log_p = log_normalize(log_p)
-
-            params.Z[row_idx, k] = discrete_rvs(np.exp(log_p))
+            params.Z[row_idx, k] = discrete_rvs_gumbel_trick(log_p)
 
         return params
